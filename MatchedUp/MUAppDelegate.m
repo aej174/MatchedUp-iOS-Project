b2b00146d42f7b0d3df96d3034bcc02a3e86ce45
aej174 <aej174@icloud.com>
2014-07-09 08:13:09 -0400

    first commit

--- MatchedUp/MUAppDelegate.m
+++ MatchedUp/MUAppDelegate.m
@@ -13,15 +13,24 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
+    [Parse setApplicationId:@"l8N4i5qonXg3R09ZuMMT7qrGENj9UIn6mWwhHvV3"
+                  clientKey:@"PO5b8Szj4GCRCbZq3Y8HGt53kfQeUWOOdDTKXk3Z"];
+    [PFFacebookUtils initializeFacebook];
+    
     return YES;
 }
-							
+
+-(BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation
+{
+    return [PFFacebookUtils handleOpenURL:url];
+}
+/*
 - (void)applicationWillResignActive:(UIApplication *)application
 {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
 }
-
+*/
 - (void)applicationDidEnterBackground:(UIApplication *)application
 {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
